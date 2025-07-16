e82a1ca0902f6c985fe4cd8a067db1a66fbd06aa
Mr-Naf <naflymma@gmail.com>
2025-07-16 19:37:13 +0530

    small change

--- src/prectice/prectice.java
+++ src/prectice/prectice.java
@@ -1,18 +1,44 @@
 package prectice;
 
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class prectice {
-  String url="https://rahulshettyacademy.com/AutomationPractice/";
+   static String url="https://rahulshettyacademy.com/AutomationPractice/";
   
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver-win64\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		try {
-			driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+			driver.get(url);
 			
+			WebElement radioButton = driver.findElement(By.xpath("//input[@type='radio' and @value='radio2']"));
+			radioButton.click();
 			
+			// Type into the Suggession box
+			WebElement Suggession = driver.findElement(By.id("autocomplete"));
+			Suggession.sendKeys("Sri");
+			//
+			 WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		        List<WebElement> suggestions = wait.until(
+		        		ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(".ui-menu-item")));
+            // find the element usinf  enhanced for loop
+	        for (WebElement suggestion : suggestions) {
+	            if (suggestion.getText().equalsIgnoreCase("Sri Lanka")) {
+	                suggestion.click();
+	                break;
+	            }
+	        }
+
+
+
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
